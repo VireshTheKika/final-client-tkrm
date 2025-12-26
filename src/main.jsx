12895dde5672bc48bc50f2a4d60275b0a200fdce
VireshWork <patilviresh077@gmail.com>
2025-12-26 17:48:26 +0530

    added toas

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ToastContainer } from "react-toastify";
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={clientId}>
+    <ToastContainer position="top-right" autoClose={3000} />
     <App />
   </GoogleOAuthProvider>
 );
